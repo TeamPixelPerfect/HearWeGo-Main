d2810391ce4fb435025f5df217a4b85448ddc63e
Damidu Thathsara <damiduthathsara427@gmail.com>
2024-02-11 18:43:01 +0530

    changed 3rd time

--- hearwego/app/artist/publicRelationCampaigns/page.tsx
+++ hearwego/app/artist/publicRelationCampaigns/page.tsx
@@ -1,7 +1,56 @@
+"use client";
 import React from "react";
+import { BorderBox, TabItem, TabsNav } from "../../styles/pressRelease.style";
+import SinglePRCampaign from "../../components/SinglePRCampaign";
+import Button from "@mui/material/Button";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import ScheduleIcon from "@mui/icons-material/Schedule";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 
-const ArtistPRCampaigns = () => {
-  return <div>Artist Dashboard PR Campaigns</div>;
-};
 
-export default ArtistPRCampaigns;
+export default function Context() {
+  const [tabIndex, setTabIndex] = React.useState(0);
+  return (
+    <BorderBox>
+      <Box sx={{ display: "flex", backgroundColor: "", padding: "10px" }}>
+        <TabsNav
+          textColor="inherit"
+          value={tabIndex}
+          onChange={(e, index) => setTabIndex(index)}
+        >
+          <TabItem disableRipple label={"In Progress"} />
+          <TabItem disableRipple label={"Completed"} />
+          <TabItem disableRipple label={"Drafts"} />
+          <TabItem disableRipple label={"Scheduled Posts"} />
+        </TabsNav>
+        <Stack direction="row" spacing={4} sx={{ padding: "5px 20px" }}>
+          <Button
+            variant="contained"
+            startIcon={<EditNoteIcon />}
+            sx={{ width: "180px" }}
+          >
+            Create Event
+          </Button>
+          <Button
+            variant="contained"
+            startIcon={<ScheduleIcon />}
+            sx={{ width: "190px" }}
+          >
+            Post Scheduling
+          </Button>
+        </Stack>
+      </Box>
+      <Box sx={{display:"flex",flexWrap:"wrap"}}>
+        <SinglePRCampaign />
+        <SinglePRCampaign />
+        <SinglePRCampaign />
+        <SinglePRCampaign />
+        <SinglePRCampaign/>
+        <SinglePRCampaign />
+        <SinglePRCampaign />
+        <SinglePRCampaign />
+      </Box>
+    </BorderBox>
+  );
+}
