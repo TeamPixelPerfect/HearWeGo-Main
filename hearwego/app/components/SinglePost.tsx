493bf938793fb8d8787b81b5cf4d4b478d72da65
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-02-13 21:48:14 +0530

    Post image changed

--- hearwego/app/components/SinglePost.tsx
+++ hearwego/app/components/SinglePost.tsx
@@ -54,7 +54,7 @@ import {
 
           <PostImageCard
             image={
-              "https://images.unsplash.com/photo-1517147177326-b37599372b73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2229&q=80"
+              "https://nsidc.org/sites/default/files/images/After%20a%20snow%20storm%2C%20the%20sun%20emerged%20revealing%20this%20beautiful%20scene%20on%20Cedar%20River%20Road%20west%20of%20Mount%20Vernon%2C%20IA..jpg"
             }
           />
           <Box
