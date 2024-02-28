49b547f1a1691857298c69460fd05bd21d6dcfb5
dasun-madusanka <dasunmadusanka7890@gmail.com>
2024-02-28 21:55:57 +0530

    Added platform link section to the add song preview page

--- hearwego/app/artist/songs/addSongPreview/page.tsx
+++ hearwego/app/artist/songs/addSongPreview/page.tsx
@@ -11,13 +11,22 @@ import CardMedia from "@mui/material/CardMedia";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import TextField from "@mui/material/TextField";
 import LockIcon from "@mui/icons-material/Lock";
 import Alert from "@mui/material/Alert";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { FaFacebook } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 import { FaXTwitter } from "react-icons/fa6";
+import Grid from "@mui/material/Unstable_Grid2";
 import { MdHeadset } from "react-icons/md";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import IconButton from "@mui/material/IconButton";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { FaSpotify } from "react-icons/fa";
 import React from "react";
 
 import {
@@ -28,11 +37,75 @@ import {
 
 export default function AddSongPreview() {
   return (
-    <Box sx={{ width: "100%", height: "75vh" }}>
-      <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ width: "100%", marginBottom: "1em" }}>
         <Card variant="outlined">{NewSongPreviewCard}</Card>
       </Box>
+      <Box sx={{ width: "100%" }}>
+        <Card variant="outlined">{NewSongPlatformLinkCard}</Card>
+      </Box>
     </Box>
+  );
+}
+
+function PlatformLinkClips() {
+  const handleClick = () => {
+    console.info("You clicked the Chip.");
+  };
+
+  const handleDelete = () => {
+    console.info("You clicked the delete icon.");
+  };
+  return (
+    <>
+      <Stack
+        sx={{ display: "flex", alignItems: "center", marginTop: "1em" }}
+        direction="row"
+        spacing={1}
+      >
+        <Box
+          sx={{
+            width: 32,
+            fontSize: 32,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FaSpotify />
+        </Box>
+
+        <Chip
+          label="https://open.spotify.com/playlist/37i9dQZF1DWWY64wDtewQt?si=8e46f716cca54567"
+          variant="outlined"
+          onClick={handleClick}
+          onDelete={handleDelete}
+        />
+      </Stack>
+
+      <Stack
+        sx={{ display: "flex", alignItems: "center", marginTop: "1em" }}
+        direction="row"
+        spacing={1}
+      >
+        <Box
+          sx={{
+            width: 32,
+            fontSize: 32,
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <FaSpotify />
+        </Box>
+
+        <Chip
+          label="https://open.spotify.com/playlist/37i9dQZF1DWWY64wDtewQt?si=8e46f716cca54567"
+          variant="outlined"
+          onClick={handleClick}
+          onDelete={handleDelete}
+        />
+      </Stack>
+    </>
   );
 }
 
@@ -47,9 +120,92 @@ const NewSongPreviewCard = (
       </Typography>
 
       <SongPreview />
+
+      <Typography
+        sx={{ fontSize: 14, marginTop: "1em" }}
+        color="text.secondary"
+        gutterBottom
+      >
+        By uploading, you confirm that your sounds comply with our Terms of Use
+        and you don't infringe anyone else's rights.
+      </Typography>
     </CardContent>
   </React.Fragment>
 );
+
+const NewSongPlatformLinkCard = (
+  <React.Fragment>
+    <CardContent>
+      <Typography variant="h5" component="div" sx={{ marginBottom: "1em" }}>
+        Streaming Platform Links
+      </Typography>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={3} sx={{}}>
+            <SelectPlatform />
+          </Grid>
+          <Grid xs={7}>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { width: "100%" },
+              }}
+              noValidate
+              autoComplete="off"
+              style={{ boxSizing: "initial" }}
+            >
+              <TextField
+                id="filled-basic"
+                label="Platform Link"
+                variant="filled"
+              />
+            </Box>
+          </Grid>
+
+          <Grid xs={2}>
+            <IconButton aria-label="delete" size="large">
+              <AddCircleIcon fontSize="inherit" />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <PlatformLinkClips />
+    </CardContent>
+  </React.Fragment>
+);
+
+function SelectPlatform() {
+  const [platform, setPlatform] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setPlatform(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl variant="filled" sx={{ minWidth: "100%" }}>
+        <InputLabel id="demo-simple-select-filled-label">Platform</InputLabel>
+        <Select
+          labelId="demo-simple-select-filled-label"
+          id="demo-simple-select-filled"
+          value={platform}
+          onChange={handleChange}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>
+            {" "}
+            <FaSpotify /> Spotify
+          </MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
 
 function SongPreview() {
   return (
@@ -116,8 +272,20 @@ function SongPreview() {
       </SongPreviewDetails>
 
       <SongPreviewShare>
-        <Box sx={{ width: "100%", display: 'flex', flexDirection:'column', alignItems: 'center', paddingTop:'1em'}}>
-          <Typography variant="h6" component="div" sx={{ fontSize: 18, marginBottom:'1em' }}>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            paddingTop: "1em",
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ fontSize: 18, marginBottom: "1em" }}
+          >
             Share your new Track
           </Typography>
           <Stack
@@ -131,7 +299,14 @@ function SongPreview() {
             <MdHeadset />
           </Stack>
         </Box>
-        <Box sx={{ width: "100%", backgroundColor: "primary.main" , padding: '5px', borderTopLeftRadius: '20px'}}>
+        <Box
+          sx={{
+            width: "100%",
+            backgroundColor: "primary.main",
+            padding: "5px",
+            borderTopLeftRadius: "20px",
+          }}
+        >
           <Stack direction="row" spacing={1}>
             <Box>https://www.hearwego.com/wq23s</Box>
             <ContentCopyIcon />
