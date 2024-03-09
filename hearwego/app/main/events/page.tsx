7e26ff0d6d8f28d31ae20d6da6bcccbdc849ee87
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-03-09 15:58:45 +0530

    MW-Events Page_02

--- hearwego/app/main/events/page.tsx
+++ hearwego/app/main/events/page.tsx
@@ -3,6 +3,12 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 
+import InputBase from "@mui/material/InputBase";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+
+
+
 import {
   Maindiv,
   CoverCardMedia,
@@ -10,9 +16,64 @@ import {
   Caption01Box,
   Caption02Box,
   Caption03Box,
+  SearchPaper,
+  CustomSelect,
 } from "@/app/styles/eventsMW.styles";
 
+export const TypeOptions = [
+  { value: "pop", label: "Pop" },
+  { value: "rock", label: "Rock" },
+  { value: "hiphop", label: "Hip Hop" },
+  { value: "jazz", label: "Jazz" },
+  { value: "classical", label: "Classical" },
+];
+
+export const LocationOptions = [
+  { value: "singer", label: "Singer" },
+  { value: "guitarist", label: "Guitarist" },
+  { value: "drummer", label: "Drummer" },
+  { value: "pianist", label: "Pianist" },
+  { value: "bassist", label: "Bassist" },
+];
+
+export const ArtistOptions = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "other", label: "Other" },
+];
+
+
+
+
+
 export default function EventsPage() {
+  const [Type, setType] = React.useState("");
+  const [Location, setLocation] = React.useState("");
+  const [Artist, setArtist] = React.useState("");
+
+  
+
+  const handleTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setType(event.target.value as string);
+  };
+
+  const handleLocationChange = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
+    setLocation(event.target.value as string);
+  };
+
+  const handleArtistChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setArtist(event.target.value as string);
+  };
+
+  const [value, setValue] = React.useState(0);
+  const handleScrollChange = (
+    event: React.SyntheticEvent,
+    newValue: number
+  ) => {
+    setValue(newValue);
+  };
   return (
     <Maindiv>
       <CoverCardMedia image="https://hwgbucket.s3.ap-south-1.amazonaws.com/images/_9b375a93-861d-4808-be51-555278ec9e21.jpeg">
@@ -40,6 +101,85 @@ export default function EventsPage() {
           </Box>
         </CaptionBox>
       </CoverCardMedia>
+
+      
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          backgroundColor: "blue",
+          //padding: "20px",
+        }}
+      >
+      <Box
+        style={{
+          width: "50%",
+          display: "flex",
+          padding: "15px",
+          marginLeft: "40px",
+          backgroundColor: "red",
+         // justifyContent: "left",
+        }}
+      >
+        <SearchPaper>
+          <InputBase
+            sx={{ ml: 5, flex: 1 }}
+            placeholder="Search for Events"
+            inputProps={{ "aria-label": "search" }}
+          />
+          <IconButton type="button" sx={{ p: "10" }} aria-label="Search">
+            <SearchIcon />
+          </IconButton>
+            
+        </SearchPaper>
+      </Box>
+
+    <Box sx={{
+      width: "50%",
+      backgroundColor: "white",
+      position: "relative",
+      display: "flex",
+      justifyContent: "right",
+      //alignItems:'right',
+      //padding: "15px",
+      margin:'15px 40px 15px 0px',
+    }}>
+      <Stack direction="row" spacing={5}>
+          <CustomSelect
+            labelId="Type-select-label"
+            id="Type-select"
+            value={Type}
+            onChange={handleTypeChange}
+            label="Type"
+            options={TypeOptions}
+            placeholder="Type"
+          />
+          <CustomSelect
+            labelId="Location-select-label"
+            id="Location-select"
+            value={Location}
+            onChange={handleLocationChange}
+            label="Location"
+            options={LocationOptions}
+            placeholder="Location"
+          />
+          <CustomSelect
+            labelId="Artist-select-label"
+            id="Artist-select"
+            value={Artist}
+            onChange={handleArtistChange}
+            label="Artist"
+            options={ArtistOptions}
+            placeholder="Artist"
+          />
+          
+          
+        </Stack>
+        </Box>
+      </Box>
     </Maindiv>
   );
 }
--- hearwego/app/styles/eventsMW.styles.tsx
+++ hearwego/app/styles/eventsMW.styles.tsx
@@ -3,6 +3,26 @@ import * as React from "react";
 import { styled } from "@mui/material/styles";
 import CardMedia from "@mui/material/CardMedia";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+interface CustomSelectProps {
+  labelId: string;
+  id: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<{ value: unknown }>) => void;
+  label: string;
+  options: Option[];
+  placeholder?: string;
+}
 
 export const Maindiv = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
@@ -63,3 +83,51 @@ export const Caption03Box = styled(Box)(({ theme }) => ({
     fontWeight: "bold",
     color: "white",
   }));
+
+  export const SearchPaper = styled(Paper)(({ theme }) => ({
+    component: "form",
+    p: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: "80%",
+    backgroundColor: theme.palette.background.default,
+    borderRadius: "90px",
+    height: "35px",
+    position: "relative",
+  }));
+
+  export const CustomSelect: React.FC<CustomSelectProps> = ({
+    labelId,
+    id,
+    value,
+    onChange,
+    label,
+    options,
+  }) => {
+    return (
+      <div>
+        <InputLabel id={labelId}>{label}</InputLabel>
+        <Select
+          labelId={labelId}
+          id={id}
+          value={value}
+          onChange={(event: React.ChangeEvent<{ value: string }>) =>
+            onChange(event)
+          }
+          label={label}
+          sx={{
+            width: "100px",
+            backgroundColor: "secondary.main",
+            color: "background.default",
+            height: "40px",
+          }}
+        >
+          {options.map((option, index) => (
+            <MenuItem key={index} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </div>
+    );
+  };
