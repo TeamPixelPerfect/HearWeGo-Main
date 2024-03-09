48b73a76cd260535f0bbe7ab0ec4ab9ab1ef7e69
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-03-09 14:53:18 +0530

    MW-Events Page_01

--- hearwego/app/main/events/page.tsx
+++ hearwego/app/main/events/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client";
+import React from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
-const Events = () => {
+import {
+  Maindiv,
+  CoverCardMedia,
+  CaptionBox,
+  Caption01Box,
+  Caption02Box,
+  Caption03Box,
+} from "@/app/styles/eventsMW.styles";
+
+export default function EventsPage() {
   return (
-    <div>
-      Events
-    </div>
-  )
-}
+    <Maindiv>
+      <CoverCardMedia image="https://hwgbucket.s3.ap-south-1.amazonaws.com/images/_9b375a93-861d-4808-be51-555278ec9e21.jpeg">
+        <div
+          style={{
+            background: "black",
+            height: "500px",
+            width: "100%",
+            opacity: "0.7",
+          }}
+        ></div>
 
-export default Events
+        <CaptionBox>
+          <Caption01Box>Book your Ticket</Caption01Box>
+          <Box
+            sx={{
+              height: "100%",
+              position: "relative",
+              display: "flex",
+              // backgroundColor: "blue",
+            }}
+          >
+            <Caption02Box>for your Favorite Event </Caption02Box>
+            <Caption03Box>Now</Caption03Box>
+          </Box>
+        </CaptionBox>
+      </CoverCardMedia>
+    </Maindiv>
+  );
+}
---
+++ hearwego/app/styles/eventsMW.styles.tsx
@@ -0,0 +1,65 @@
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import CardMedia from "@mui/material/CardMedia";
+import Box from "@mui/material/Box";
+
+export const Maindiv = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  width: "100%",
+  height: "100%",
+}));
+
+export const CoverCardMedia = styled(CardMedia)(({ theme }) => ({
+  width: "100%",
+  height: "500px",
+  marginTop: "0px",
+  position: "relative",
+  display: "flex",
+  backgroundSize: "cover",
+    backgroundPosition: "center",
+}));
+
+export const CaptionBox = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  display: "flex",
+  width: "90%",
+  height: "35%",
+  padding: "1em",
+  margin: "0px 0px 0px 2%",
+  //backgroundColor:'blue',
+   top: "60%",
+   flexDirection: "column",
+}));
+
+export const Caption01Box = styled(Box)(({ theme }) => ({
+  position: "relative",
+  display: "flex",
+  fontSize: "56px",
+  //fontFamily: "Roboto",
+  fontWeight: "bold",
+  color:"white",
+  textAlign: "center",
+  //backgroundColor:'yellow'
+}));
+
+export const Caption02Box = styled(Box)(({ theme }) => ({
+  height: "100%",  
+  padding: "8px 0px 0px 0px",
+  position: "relative",
+  display: "flex",
+  fontSize: "32px",
+  //fontFamily: "Roboto",
+  fontWeight: "semi-bol",
+  color: "#A5B4FC",
+  textAlign: "center",
+  //backgroundColor:'red'
+}));
+export const Caption03Box = styled(Box)(({ theme }) => ({
+     height: "100%",
+    //backgroundColor: "black",
+    fontSize: "40px",
+    marginLeft: "8px",
+    fontWeight: "bold",
+    color: "white",
+  }));
