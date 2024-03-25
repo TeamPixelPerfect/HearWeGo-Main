87e277e634029277a7e7b8b9f0a462161b43adc6
Damidu Thathsara <damiduthathsara427@gmail.com>
2024-03-25 21:04:08 +0530

    phone input updated

--- hearwego/app/main/events/BuyTickets/page.tsx
+++ hearwego/app/main/events/BuyTickets/page.tsx
@@ -277,15 +277,18 @@ export default function Page() {
         }}
       >
         {allStepsCompleted() ? (
-          <React.Fragment>
-            <Typography sx={{ mt: 2, mb: 1 }}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-              <Box sx={{ flex: "1 1 auto" }} />
-              <Button onClick={handleReset}>Reset</Button>
-            </Box>
-          </React.Fragment>
+          <Box>
+            Completed
+          </Box>
+          // <React.Fragment>
+          //   <Typography sx={{ mt: 2, mb: 1 }}>
+          //     All steps completed - you&apos;re finished
+          //   </Typography>
+          //   <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+          //     <Box sx={{ flex: "1 1 auto" }} />
+          //     <Button onClick={handleReset}>Reset</Button>
+          //   </Box>
+          // </React.Fragment>
         ) : (
           <React.Fragment>
             <Typography sx={{ mt: 2, mb: 1, py: 1 }}>
