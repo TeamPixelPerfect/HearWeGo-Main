ec811f756ee0bccb725063eaf3648f14217d3941
Himesh Shiwantha <45970959+ShiwanthaH@users.noreply.github.com>
2024-03-28 18:18:28 +0530

    Updated Base URL

--- hearwego/app/constants/keys.tsx
+++ hearwego/app/constants/keys.tsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 // export const base_url = "http://localhost:5000/api";
 export const base_url = "http://3.0.89.51:5000//api";
-=======
-// baseUrl for the API
-export const base_url = "http://localhost:5000/api";
->>>>>>> 05c6747cbf11e99502bf3c78f6d63d6e344c744d
+
