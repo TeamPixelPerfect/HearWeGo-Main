580a7fd7c79591b870257f55d5caad55239b07df
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-05-29 22:58:06 +0530

    Press Release_03

--- hearwego/app/artist/pressRelease/page.tsx
+++ hearwego/app/artist/pressRelease/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 import Box from "@mui/material/Box";
 import TabContext from "@mui/lab/TabContext";
@@ -12,18 +12,104 @@ import { DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import LoadingButton from "@mui/lab/LoadingButton";
 import SaveIcon from "@mui/icons-material/Save";
 import Button from "@mui/material/Button";
+import LoadingButton from "@mui/lab/LoadingButton";
 import Stack from "@mui/material/Stack";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
 import { TabsNav, TabItem } from "../../styles/PressReleaseOriginal.styles";
 
 export default function PressRelease() {
-  const [songFile, setSongFile] = React.useState(null);
+  const [headline, setHeadline] = useState("");
+  const [subHeadline, setSubHeadline] = useState("");
+  const [venue, setVenue] = useState("");
+  const [date, setDate] = useState(null);
+  const [description, setDescription] = useState("");
+  const [releaseDate, setReleaseDate] = useState(null);
+  const [songFile, setSongFile] = useState(null);
+  const [value, setValue] = useState("1");
+  const [openCancelDialog, setOpenCancelDialog] = useState(false);
+  const [openSaveDialog, setOpenSaveDialog] = useState(false);
+  const [openShareDialog, setOpenShareDialog] = useState(false);
+  const [openSavedItemsDialog, setOpenSavedItemsDialog] = useState(false);
+  const [openSuccessfullySharedDialog, setOpenSuccessfullySharedDialog] =
+    useState(false);
 
-  const [value, setValue] = React.useState("1");
   const handle01Change = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
+  };
+
+  const handleCancelOpen = () => {
+    setOpenCancelDialog(true);
+  };
+
+  const handleCancelClose = () => {
+    setOpenCancelDialog(false);
+  };
+
+  const handleSaveOpen = () => {
+    if (validateFields()) {
+      setOpenSaveDialog(true);
+    }
+  };
+
+  const handleSaveClose = () => {
+    setOpenSaveDialog(false);
+  };
+
+  const handleShareOpen = () => {
+    setOpenShareDialog(true);
+    setOpenSaveDialog(false);
+  };
+
+  const handleShareClose = () => {
+    setOpenShareDialog(false);
+  };
+
+  const handleSavedItemsOpen = () => {
+    setOpenSavedItemsDialog(true);
+  };
+
+  const handleSavedItemsClose = () => {
+    setOpenSavedItemsDialog(false);
+  };
+
+  const handleSuccessfullySharedOpen = () => {
+    setOpenSuccessfullySharedDialog(true);
+  };
+
+  const handleSuccessfullySharedClose = () => {
+    setOpenSuccessfullySharedDialog(false);
+  };
+
+  const handleSaveAsDraft = () => {
+    console.log("Saved in drafts");
+    handleCancelClose();
+  };
+
+  const handleSaveItem = () => {
+    console.log("The press release is saved in saved items");
+    handleSavedItemsOpen();
+    handleSaveClose();
+  };
+
+  const handleShare = () => {
+    console.log("Successfully Shared");
+    handleSuccessfullySharedOpen();
+    handleShareClose();
+  };
+
+  const validateFields = () => {
+    if (!headline || !subHeadline || !venue || !description || !releaseDate) {
+      alert("All fields are required!");
+      return false;
+    }
+    return true;
   };
 
   return (
@@ -115,6 +201,9 @@ export default function PressRelease() {
                       id="Press_Headline"
                       label="Headline"
                       variant="filled"
+                      value={headline}
+                      onChange={(e) => setHeadline(e.target.value)}
+                      required
                     />
                   </Box>
                   <Box
@@ -132,6 +221,9 @@ export default function PressRelease() {
                       id="Press_SubHeadline"
                       label="Sub Headline"
                       variant="filled"
+                      value={subHeadline}
+                      onChange={(e) => setSubHeadline(e.target.value)}
+                      required
                     />
                   </Box>
                   {/* Date Picker */}
@@ -151,10 +243,9 @@ export default function PressRelease() {
                       <DemoContainer components={[" DatePicker"]}>
                         <DatePicker
                           label="Date"
-                          onChange={(value) => {
-                            const date = value.format("YYYY-MM-DD").toString();
-                            console.log(date);
-                          }}
+                          value={date}
+                          onChange={(value) => setDate(value)}
+                          required
                         />
                       </DemoContainer>
                     </LocalizationProvider>
@@ -176,6 +267,9 @@ export default function PressRelease() {
                       id="PressRelease_Venue"
                       label="Venue"
                       variant="filled"
+                      value={venue}
+                      onChange={(e) => setVenue(e.target.value)}
+                      required
                     />
                   </Box>
 
@@ -195,10 +289,10 @@ export default function PressRelease() {
                       label="Description"
                       multiline
                       rows={4}
-                      //defaultValue="Description of the Press Release"
                       variant="filled"
-                      // Handler for updating album description in state
-                      onChange={(e) => {}}
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      required
                     />
                   </Box>
 
@@ -260,12 +354,9 @@ export default function PressRelease() {
                         <DemoContainer components={[" DatePicker"]}>
                           <DatePicker
                             label="Release Date"
-                            onChange={(value) => {
-                              const date = value
-                                .format("YYYY-MM-DD")
-                                .toString();
-                              console.log(date);
-                            }}
+                            value={releaseDate}
+                            onChange={(value) => setReleaseDate(value)}
+                            required
                           />
                         </DemoContainer>
                       </LocalizationProvider>
@@ -273,6 +364,7 @@ export default function PressRelease() {
                   </Box>
                 </Box>
               </Box>
+
               <div
                 style={{
                   display: "flex",
@@ -281,14 +373,133 @@ export default function PressRelease() {
                 }}
               >
                 <Stack direction="row" spacing={2}>
-                  <Button variant="outlined" onClick={() => {}}>
-                    Cancal
+                  <Button variant="outlined" onClick={handleCancelOpen}>
+                    Cancel
                   </Button>
-                  <LoadingButton startIcon={<SaveIcon />} variant="contained">
+                  <LoadingButton
+                    startIcon={<SaveIcon />}
+                    variant="contained"
+                    onClick={handleSaveOpen}
+                  >
                     Save
                   </LoadingButton>
                 </Stack>
               </div>
+
+              <Dialog
+                open={openCancelDialog}
+                onClose={handleCancelClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {"Save as Draft?"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    Do you want to save this press release as a draft?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleCancelClose}>No</Button>
+                  <Button onClick={handleSaveAsDraft} autoFocus>
+                    Yes
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <Dialog
+                open={openSaveDialog}
+                onClose={handleSaveClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+              >
+                <DialogTitle id="alert-dialog-title">
+                  {"Share Press Release?"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="alert-dialog-description">
+                    Do you want to share this press release now?
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleSaveItem}>No</Button>
+                  <Button onClick={handleShareOpen} autoFocus>
+                    Yes
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <Dialog
+                open={openShareDialog}
+                onClose={handleShareClose}
+                aria-labelledby="share-dialog-title"
+                aria-describedby="share-dialog-description"
+              >
+                <DialogTitle id="share-dialog-title">
+                  {"Share Options"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="share-dialog-description">
+                    Choose your sharing options here.
+                  </DialogContentText>
+                  {/* Add your share options here */}
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleShareClose}>Cancel</Button>
+                  <Button
+                    onClick={() => {
+                      handleShare();
+                      handleShareClose();
+                    }}
+                    autoFocus
+                  >
+                    Share
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <Dialog
+                open={openSavedItemsDialog}
+                onClose={handleSavedItemsClose}
+                aria-labelledby="saved-items-dialog-title"
+                aria-describedby="saved-items-dialog-description"
+              >
+                <DialogTitle id="saved-items-dialog-title">
+                  {"Saved"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="saved-items-dialog-description">
+                    The press release is saved in saved items.
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleSavedItemsClose} autoFocus>
+                    OK
+                  </Button>
+                </DialogActions>
+              </Dialog>
+
+              <Dialog
+                open={openSuccessfullySharedDialog}
+                onClose={handleSuccessfullySharedClose}
+                aria-labelledby="successfully-shared-dialog-title"
+                aria-describedby="successfully-shared-dialog-description"
+              >
+                <DialogTitle id="successfully-shared-dialog-title">
+                  {"Success"}
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="successfully-shared-dialog-description">
+                    Successfully Shared.
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleSuccessfullySharedClose} autoFocus>
+                    OK
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </div>
           </CardContent>
         </React.Fragment>
