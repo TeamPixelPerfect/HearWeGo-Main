5cc090c23d75dd3184865308d88b933d6cac9604
Himesh Shiwantha <45970959+ShiwanthaH@users.noreply.github.com>
2024-06-26 11:11:53 +0530

    UI Changes

--- hearwego/app/components/SingleEvent.tsx
+++ hearwego/app/components/SingleEvent.tsx
@@ -22,7 +22,7 @@ export default function SingleEvent({
   artistName,
 }: Props) {
   return (
-    <Link href={`/main/events/`+eventID}>
+    <Link href={`/main/events/` + eventID}>
       <Box
       // sx={{
       //   textAlign: "center",
@@ -39,13 +39,14 @@ export default function SingleEvent({
         <div
           style={{
             background: "black",
-            width: "150px",
-            height: "200px",
+            width: "100%",
+            height: "400px",
             //  width: "100%",
             //  height: "100%",
             borderRadius: "14px",
             display: "flex",
             justifyContent: "center",
+            marginBottom: "1em",
           }}
         >
           <Box
@@ -59,151 +60,170 @@ export default function SingleEvent({
               backgroundRepeat: "no-repeat",
               backgroundSize: "cover",
               borderRadius: "14px",
-              opacity: "0.4",
+              opacity: "0.7",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+
               //position:'relavant'
             }}
           >
             <Box
               sx={{
-                backgroundColor: "black",
+                backgroundColor: "rgba(0,0,0,0.7)",
                 color: "white",
                 width: "50%",
-                height: "30px",
+                // height: "30px",
                 fontSize: "16px",
-                borderRadius: "10px",
+                borderRadius: "10px 0 0 0",
                 fontWeight: "bold",
                 position: "relative",
                 margin: "5px 0px 0px 5px",
                 justifyContent: "center",
-                padding: "2px",
+                padding: "8px",
                 display: "flex",
-                opacity: "0.9",
+                // opacity: "0.9",
               }}
             >
               {eventDate}
             </Box>
 
-            <Box sx={{
-              position: "relative",
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              borderRadius: "14px",
-              //backgroundColor: "red",
-              flexDirection: "column",
-              justifyContent:'space-between',
-              alignItems:'center'
-            }}>
-              
             <Box
               sx={{
-                width: "90%",
-                height: "25%",
-                backgroundColor: "black",
-                marginTop: "70%",
                 position: "relative",
                 display: "flex",
-                borderRadius: "14px",
-                opacity: "0.8",
-                flexDirection: "row",
+                width: "100%",
+                // height: "30%",
+                borderRadius: "0",
+                //backgroundColor: "red",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                alignItems: "center",
+                padding: "4px",
               }}
             >
-              <Box sx={{
-                   width: "90%",
-                   height: "100%",
-                   //backgroundColor: "white",
-                   //marginTop: "70%",
-                   position: "relative",
-                   display: "flex",
-                   borderRadius: "14px",
-                   //opacity: "0.8",
-                   flexDirection: "column",
-              }}>
-               <Box
-                sx={{
-                  //backgroundColor: "blue",
-                  color: "white",
-                  width: "60%",
-                  height: "50%",
-                  fontSize: "16px",
-                  fontWeight: "bold",
-                  position: "relative",
-                  margin: "0px 0px 0px 5px",
-                  justifyContent: "left",
-                  padding: "2px",
-                  display: "flex",
-                  //opacity: "0.9",
-                }}
-              >
-                {eventName}
-              </Box> 
-
-              <Box sx={{
-                //backgroundColor: "green",
-                color: "white",
-                width: "100%",
-                height: "100%",
-                fontSize: "10px",
-              position: "relative",
-                margin: "0px 0px 0px 8px",
-                //justifyContent: "right",
-               // padding: "4px",
-                display: "flex",
-                //opacity: "0.9",
-              }}>
-                {artistName}
-              
-              </Box>
-              </Box>
               <Box
-              sx={{
-                //backgroundColor: "white",
-                width: "50%",
-                height: "100%",
-                 position: "relative",
-                 //padding: "10px",
-                display: "flex", 
-                alignItems:'right', 
-                //justifyContent:'space-between', 
-                padding: "6px",
-                flexDirection: "column",
-                  margin: "0px 4px 0px 0px",
-            
-                
-              }}
-              >
-                <Box
                 sx={{
-                  //backgroundColor: "red",
-                  color: "white",
-                  width: "100%",
-                  height: "30%",
-                  fontSize: "12px",
-                  //padding: "4px",
-                  margin: "0px 0px 0px 10px",
-               
-                }}
-                >
-                   {eventTime}
-                </Box>
-                <Box
-                sx={{
-                  //backgroundColor: "yellow",
-                  color: "white",
                   width: "100%",
                   height: "100%",
-                  fontSize: "10px",
+                  backgroundColor: "black",
+                  // marginTop: "70%",
                   position: "relative",
-                  margin: "0px 0px 0px 30px",
-                  //justifyContent: "left",
-                  padding: "2px",
+                  justifyContent: "center",
+                  alignItems: "center",
                   display: "flex",
+                  borderRadius: "0 0 10px 10px",
+                  opacity: "0.8",
+                  flexDirection: "row",
+                  padding: "8px 4px"
                 }}
+              >
+                <Box
+                  sx={{
+                    width: "60%",
+                    height: "100%",
+                    //backgroundColor: "white",
+                    //marginTop: "70%",
+                    position: "relative",
+                    display: "flex",
+                    borderRadius: "0",
+                    //opacity: "0.8",
+                    flexDirection: "column",
+                  }}
                 >
-                  {eventDay}
+                  <Box
+                    sx={{
+                      //backgroundColor: "blue",
+                      color: "white",
+                      width: "100%",
+                      height: "50%",
+                      fontSize: "24px",
+                      fontWeight: "bold",
+                      position: "relative",
+                      margin: "0px 0px 0px 5px",
+                      justifyContent: "left",
+                      padding: "8px",
+                      pb: "0px",
+                      pl: 0,
+                      display: "flex",
+                      //opacity: "0.9",
+                    }}
+                  >
+                    {eventName}
+                  </Box>
+
+                  <Box
+                    sx={{
+                      //backgroundColor: "green",
+                      color: "white",
+                      width: "100%",
+                      height: "100%",
+                      fontSize: "18px",
+                      position: "relative",
+                      margin: "0px 0px 0px 8px",
+                      padding: "8px",
+                      pt: "0px",
+                      pl: 0,
+                      fontWeight: 400,
+                      //justifyContent: "right",
+                      // padding: "4px",
+                      display: "flex",
+                      //opacity: "0.9",
+                    }}
+                  >
+                    {artistName}
+                  </Box>
+                </Box>
+                <Box
+                  sx={{
+                    //backgroundColor: "white",
+                    width: "30%",
+                    height: "100%",
+                    position: "relative",
+                    //padding: "10px",
+                    display: "flex",
+                    alignItems: "flex-end",
+                    //justifyContent:'space-between',
+                    // padding: "6px",
+                    flexDirection: "column",
+                    // margin: "0px 4px 0px 0px",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      //backgroundColor: "red",
+                      color: "white",
+                      width: "100%",
+                      // height: "100%",
+                      fontSize: "16px",
+                      padding: "8px",
+                      pb: 0,
+                      pr:0,
+                      // margin: "0px 0px 0px 10px",
+                    }}
+                  >
+                    {eventTime}
+                  </Box>
+                  <Box
+                    sx={{
+                      //backgroundColor: "yellow",
+                      color: "white",
+                      width: "100%",
+                      height: "100%",
+                      fontSize: "16px",
+                      position: "relative",
+                      // margin: "0px 0px 0px 30px",
+                      //justifyContent: "left",
+                      padding: "8px",
+                      pt: 0,
+                      pr:0,
+                      display: "flex",
+                    }}
+                  >
+                    {eventDay}
+                  </Box>
                 </Box>
               </Box>
-            </Box>
             </Box>
           </Box>
         </div>
