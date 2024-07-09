64baeab07ff7c99b37735f0c6e3c8e59488d0a07
Damidu Thathsara <damiduthathsara427@gmail.com>
2024-07-09 19:21:14 +0530

    Added Artist Approval

--- hearwego/app/auth/artistSignUp/page.tsx
+++ hearwego/app/auth/artistSignUp/page.tsx
@@ -23,6 +23,8 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
+  Snackbar,
+  Alert,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import CancelIcon from "@mui/icons-material/Cancel";
@@ -71,6 +73,22 @@ const ArtistSignUp = () => {
   const matches = useMediaQuery("(max-width:960px)");
   const artist = useAppSelector((state) => state.artist.user);
 
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState<"success" | "error">(
+    "success"
+  );
+
+  const handleSnackbarClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarOpen(false);
+  };
+
   // Sign up stage
   const [step, setStep] = useState<number>(0);
 
@@ -112,7 +130,7 @@ const ArtistSignUp = () => {
     "Classical",
     "World Music",
   ]);
-
+ 
   // Artist details
   const [artistDetails, setArtistDetails] = React.useState({
     artistName: "",
@@ -362,15 +380,22 @@ const ArtistSignUp = () => {
       setVerificationDocumentError(true);
       return;
     }
-    setArtistDetails({ ...artistDetails, verificationDocuments: verDoc });
+    console.log("Verification Document:::", verDoc);
+    setArtistDetails({ ...artistDetails, verificationDocuments: [verDoc] });
     setVerificationDocumentError(false);
 
     console.log("Artist Details:::", artistDetails);
 
-    handleArtistRegister(artistDetails).then((res) => {
+    handleArtistRegister({ ...artistDetails, verificationDocuments: [verDoc] }).then((res) => {
       if (res) {
-        console.log("Artist Registered Successfully! " + res);
+        setSnackbarOpen(true);
+        setSnackbarSeverity("success");
+        setSnackbarMessage("Documents submitted successfully!");
         incrementStep(1);
+      } else {
+        setSnackbarOpen(true);
+        setSnackbarSeverity("error");
+        setSnackbarMessage("Failed to submit documents!");
       }
     });
   };
@@ -2314,6 +2339,15 @@ const ArtistSignUp = () => {
           </Box>
         </Box>
       </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert onClose={handleSnackbarClose} severity={snackbarSeverity}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </AuthContainer>
   );
 };
